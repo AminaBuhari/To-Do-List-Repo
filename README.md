7f5b0ebbaea4f5e6acb38864108db8e3161d3245
buhariaminat <buhariaminat@yahoo.com>
2022-03-29 18:53:23 +0100

    Add review

--- README.md
+++ README.md
@@ -1,3 +1,34 @@
-# To-Do-List-Repo
+# Web-pack
 
-Repo
+This project is done to help understand Javascript and webpack bundles.
+
+## Built With
+
+- HTML
+- CSS
+- JavaScript
+- webpack
+
+## Live Demo
+
+For a live demo of the Portfolio [click here]().
+
+## Author
+
+👤 **Aminat Buhari**
+
+- GitHub: [@AminaBuhari](https://github.com/AminaBuhari)
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+## 📝 License
+
+This project is [MIT](./MIT.md) licensed.
---
+++ src/index.js
@@ -0,0 +1,26 @@
+import './style.css';
+
+const listElement = document.getElementById('lists-todo');
+const lists = [
+  {
+    index: 1,
+    description: 'Wash the dishes',
+    completed: false,
+  },
+  {
+    index: 2,
+    description: 'complete To Do list',
+    completed: false,
+  },
+];
+
+lists.forEach((list) => {
+  const listHTML = `<li class="list">
+<div class="listblock">
+  <input type="checkbox" name="checkbox" />
+  <p>${list.description}</p>
+</div>
+<button class="elipse"><i class="fa fa-ellipsis-v"></i></button>
+</li>`;
+  listElement.insertAdjacentHTML('beforeend', listHTML);
+});
